188ade4c22b2149d58bcc36217914341934a088b
Dishant Vyas <keenlearner13579@gmail.com>
2020-11-08 15:53:47 +0530

    Footer sorted.

--- src/components/Footers/AdminFooter.js
+++ src/components/Footers/AdminFooter.js
@@ -28,14 +28,14 @@ class Footer extends React.Component {
         <Row className="align-items-center justify-content-xl-between">
           <Col xl="6">
             <div className="copyright text-center text-xl-left text-muted">
-              Food Buddy
+              Khaana Khata
             </div>
           </Col>
 
           <Col xl="6">
             <Nav className="nav-footer justify-content-center justify-content-xl-end">
               <NavItem>
-                <NavLink href="#">Boolean Pundits</NavLink>
+                <NavLink href="#">TechTruckers</NavLink>
               </NavItem>
 
               <NavItem>
--- src/components/Footers/AuthFooter.js
+++ src/components/Footers/AuthFooter.js
@@ -30,13 +30,13 @@ class Login extends React.Component {
             <Row className="align-items-center justify-content-xl-between">
               <Col xl="6">
                 <div className="copyright text-center text-xl-left text-muted">
-                  Food Buddy
+                  Khaana Khata
                 </div>
               </Col>
               <Col xl="6">
                 <Nav className="nav-footer justify-content-center justify-content-xl-end">
                   <NavItem>
-                    <NavLink href="#">Boolean Pundits</NavLink>
+                    <NavLink href="#">TechTruckers</NavLink>
                   </NavItem>
                   <NavItem>
                     <NavLink href="#">About Us</NavLink>
